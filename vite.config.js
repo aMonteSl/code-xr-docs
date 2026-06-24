c8aee79fac9fc9e2d74d8da215f3b3d7612a6c72
Adrián Montes Linares <128893712+aMonteSl@users.noreply.github.com>
2026-06-24 20:31:30 +0200

    Set base path to '/' for all modes

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { ghPages } from 'vite-plugin-gh-pages'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: mode === 'deploy' ? [react(), ghPages()] : [react()],
-  base: mode === 'development' ? '/' : '/code-xr-docs/',
+  base: '/',
   publicDir: 'public',
   build: {
     assetsDir: 'assets',
