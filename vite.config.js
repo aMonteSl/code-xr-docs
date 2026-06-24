e3848ea7c229749b4356e37a9e8726b391540203
aMonteSl <adrian.adyra@gmail.com>
2026-06-24 21:15:19 +0200

    Remove unused GitHub Pages Vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { ghPages } from 'vite-plugin-gh-pages'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
-  plugins: mode === 'deploy' ? [react(), ghPages()] : [react()],
+export default defineConfig({
+  plugins: [react()],
   base: '/',
   publicDir: 'public',
   build: {
@@ -16,4 +15,4 @@ export default defineConfig(({ mode }) => ({
       }
     }
   }
-}))
+})
